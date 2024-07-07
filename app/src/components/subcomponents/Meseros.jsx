e60543c7a7259751772610d6349a803eb51d60a1
joaonunez <joaovaldiglesias@gmail.com>
2024-07-07 04:29:49 -0400

    proyecto finalizado2

--- app/src/components/subcomponents/Meseros.jsx
+++ app/src/components/subcomponents/Meseros.jsx
@@ -5,14 +5,14 @@ export function Meseros() {
     <div className="m-auto contenedor-meseros">
         {meseros.map((mesero)=>(
             <div key={mesero.id+mesero.nombre} className="card" style={{ width: "18rem" }}>
-            <img src={mesero.foto} className="card-img-top" alt="..." />
+            <img src={mesero.foto} className="card-img-top foto" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{mesero.nombre}</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
               </p>
-              <a href="" className="btn btn-primary">
+              <a href="www.google.com" className="btn btn-primary">
                 Enviar Propina
               </a>
             </div>
